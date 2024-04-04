fe5e53adddd88afd7ab17e8ca91f8a1b52e7760b
Luis Silva <1231869@isep.ipp.pt>
2024-04-04 23:45:43 +0100

    FIX: Closed #15 Fixed bug that stopped proper rendering and caused browser errors

--- CA1/tut-react-and-spring-data-rest/basic/src/main/js/app.js
+++ CA1/tut-react-and-spring-data-rest/basic/src/main/js/app.js
@@ -37,7 +37,6 @@ class EmployeeList extends React.Component{
 		return (
 			<table>
 				<tbody>
-<<<<<<< HEAD
 				<tr>
 					<th>First Name</th>
 					<th>Last Name</th>
@@ -47,17 +46,6 @@ class EmployeeList extends React.Component{
 					<th>Email</th>
 				</tr>
 				{employees}
-=======
-					<tr>
-						<th>First Name</th>
-						<th>Last Name</th>
-						<th>Description</th>
-						<th>Job Title</th>
-						<th>Job Years</th>
-						<th>Email</th>
-					</tr>
-					{employees}
->>>>>>> email-field
 				</tbody>
 			</table>
 		)
@@ -75,7 +63,7 @@ class Employee extends React.Component{
 				<td>{this.props.employee.description}</td>
 				<td>{this.props.employee.jobTitle}</td>
 				<td>{this.props.employee.jobYears}</td>
-				<td>{this.props.employees.email}</td>
+				<td>{this.props.employee.email}</td>
 			</tr>
 		)
 	}
