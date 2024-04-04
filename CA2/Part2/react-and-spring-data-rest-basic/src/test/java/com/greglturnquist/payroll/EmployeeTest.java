8d5182d1b3bc179f7cd1c982ad92f2a15097cc8f
Luis Silva <1231869@isep.ipp.pt>
2024-04-04 11:50:28 +0100

    FEAT: Closed #13. Setup CA2/Part2 and ran initial commands to verify functionality

---
+++ CA2/Part2/react-and-spring-data-rest-basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
@@ -0,0 +1,73 @@
+package com.greglturnquist.payroll;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+    private String validEmployeeFirstName = "John";
+    private String nullString = null;
+    private String emptyString = "";
+    private String validEmployeeLastName = "Man";
+    private String validJobDescription = "Man with family";
+    private String validJobTitle = "Family man";
+    private int validJobYears = 10;
+    private int negativeJobYears = -10;
+    private String validEmail = "john_123@family.man";
+
+    /**
+     * Test construction of new Employee obj with valid parameters. Sees if illegalArgumentException isn't thrown
+     */
+    @Test
+    void constructEmployeeWithValidAttributes() {
+        assertDoesNotThrow(() -> new Employee(validEmployeeFirstName, validEmployeeLastName, validJobDescription, validJobTitle, validJobYears, validEmail));
+    }
+
+    /**
+     * Test construction of new Employee obj with nullString as first name and rest valid arguments. Sees if IllegalArgumentException is thrown
+     */
+    @Test
+    void constructEmployeeNullFirstName() {
+        assertThrows(IllegalArgumentException.class, () -> new Employee(nullString, validEmployeeLastName, validJobDescription, validJobTitle, validJobYears, validEmail));
+    }
+
+    /**
+     * Test construction of new Employee obj with emptyString as last name and rest valid arguments. Sees if IllegalArgumentException is thrown
+     */
+    @Test
+    void constructEmployeeEmptyLastName() {
+        assertThrows(IllegalArgumentException.class, () -> new Employee(validEmployeeFirstName, emptyString, validJobDescription, validJobTitle, validJobYears, validEmail));
+    }
+
+    /**
+     * Test construction of new Employee obj with negativeJobYears and rest valid arguments. Sess if IllegalArgumentException is thrown
+     */
+    @Test
+    void constructEmployeeWithNegativeJobYears() {
+        assertThrows(IllegalArgumentException.class, () -> new Employee(validEmployeeFirstName, validEmployeeLastName, validJobDescription, validJobTitle, negativeJobYears, validEmail));
+    }
+
+    /**
+     * Test construction of new Employee obj with emptyString as email and rest valid arguments. See if IllegalArgumentException is thrown
+     */
+    @Test
+    void constructEmployeeWithEmptyEmail() {
+        assertThrows(IllegalArgumentException.class, () -> new Employee(validEmployeeFirstName, validEmployeeLastName, validJobDescription, validJobTitle, validJobYears, emptyString));
+    }
+
+    /**
+     * Test construction of new Employee obj with nullString as email and rest valid arguments. See if IllegalArgumentException is thrown
+     */
+    @Test
+    void constructEmployeeWithNullEmail() {
+        assertThrows(IllegalArgumentException.class, () -> new Employee(validEmployeeFirstName, validEmployeeLastName, validJobDescription, validJobTitle, validJobYears, nullString));
+    }
+
+    /**
+     * Test construction of new Employee obj with email that does not contain the "@" character
+     */
+    @Test
+    void constructEmployeeWithInvalidEmail() {
+        assertThrows(IllegalArgumentException.class, () -> new Employee(validEmployeeFirstName, validEmployeeLastName, validJobDescription, validJobTitle, validJobYears, "john_123family.man"));
+    }
+}
---
+++ CA2/Part2/readme.md
@@ -0,0 +1,9 @@
+Create branch using git branch
+Go to branch using checkout
+Generate project base using spring io
+Copy over to folder
+Run ./gradlew tasks and ./gradlew tasks --all to see all tasks
+Exclude called folders
+Change import in employee.class from javax to jakarta
+We build the application using ./gradlew bootrun
+Check localhost:8080 to see it is running/empty
